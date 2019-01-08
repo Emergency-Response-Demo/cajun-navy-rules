c3a63c1a51733b4ed7324adbe964630bfd4ad10a
Michael Reynolds <mireynol@redhat.com>
2019-01-08 16:49:41 -0500

    project skeleton w/ JUnit 5 test (#1)

---
+++ src/test/java/com/redhat/cajun/navy/rules/IncidentResponderAssignmentRulesTest.java
@@ -0,0 +1,19 @@
+package com.redhat.cajun.navy.rules;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@DisplayName( "Cajun Navy Incident Assignement Planner Tests" )
+public class IncidentResponderAssignmentRulesTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger( IncidentResponderAssignmentRulesTest.class );
+
+    @Test
+    @DisplayName( "My first test" )
+    public void myExampleTest() {
+        LOG.info( "My first test" );
+    }
+
+}
